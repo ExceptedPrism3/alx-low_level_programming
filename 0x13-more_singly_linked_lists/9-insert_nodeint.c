006c878d86f985134e125863db761d47ba1239ce
ExceptedPrism3 <ben.ayoube@outlook.com>
2023-04-29 07:08:29 -0700

    9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,44 +1,43 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - insert node at index
- * @head: linked list
- * @idx: posisiton to insert
- * @n: data
- * Return: address of new node or null if failed
+ * insert_nodeint_at_index - inserts a new node at a given position
+ *
+ * @head: double pointer to head node
+ * @idx: index at which to insert new node
+ * @n: int data value for new node
+ * Return: address of new node or NULL if operation fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *tmp;
-	listint_t *tmp2;
-	listint_t *len;
-	unsigned int length = 0, i = 0;
+	listint_t *new_node, *temp;
+	unsigned int i = 0;
 
-	len = *head;
-	while (len)
-	{
-		length++;
-		len = len->next;
-	}
-
-	if (idx > length)
+	if (!head)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL;
-	tmp = *head;
-
-	while (tmp && i < idx - 1)
+	if (idx == 0)
 	{
-		tmp = tmp->next;
-		i++;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	tmp2 = tmp;
-	tmp = new_node;
-	new_node = tmp2;
-	return (tmp2);
+	temp = *head;
+	while (i < (idx - 1))
+	{
+		temp = temp->next;
+		i++;
+		if (temp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
